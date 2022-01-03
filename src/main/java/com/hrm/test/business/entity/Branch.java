33db38cc9dd9f2bc05f42687a8c2e5c201d83d34
mohamed-aly <mali902016@gmail.com>
2022-01-03 14:15:55 +0200

    added entities attributes and basic validation

--- src/main/java/com/hrm/test/business/entity/Branch.java
+++ src/main/java/com/hrm/test/business/entity/Branch.java
@@ -1,4 +1,27 @@
 package com.hrm.test.business.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Branch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Pattern(regexp = "^[\\u0600-\\u06FF]+$", message = "Invalid Name!")
+    private String name;
 }
--- src/main/java/com/hrm/test/business/entity/Employee.java
+++ src/main/java/com/hrm/test/business/entity/Employee.java
@@ -1,4 +1,34 @@
 package com.hrm.test.business.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Pattern(regexp = "^[\\u0600-\\u06FF]+$", message = "Invalid Name!")
+    private String name;
+
+    @Column(unique = true)
+    @Size(max = 14, min = 14)
+    private String nationalId;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Branch branch;
 }
